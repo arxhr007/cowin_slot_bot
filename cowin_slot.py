75e960a4f22a4b3e54720a1641c20443014d797f
Aaron Thomas <84493696+BLINKING-IDIOT@users.noreply.github.com>
2022-02-08 18:35:33 +0530

    Create cowin_slot.py

---
+++ cowin_slot.py
@@ -0,0 +1,27 @@
+import requests
+from pytz import timezone 
+from datetime import datetime
+
+ind_time = datetime.now(timezone("Asia/Kolkata")).strftime('%d-%m-%Y')
+x=requests.get("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=303&date="+ind_time)
+dit=x.json()
+dit=dit["sessions"]
+z={}
+sen=""
+for i in dit:
+    sen+="\nname:"+i["name"]
+    sen+="\naddress="+i["address"]
+    sen+="\nblock="+i["block_name"]
+    sen+="\nfee type="+i["fee_type"]
+    sen+="\ntime:"
+    for j in i["slots"]:
+        sen+="\n"+j
+    sen+="\n\n"
+print(sen)
+print("full details:\n\n")
+sen2=""
+for i in dit:
+    for j in i:
+        sen2+="\n"+j+"="+str(i[j])
+    sen2+="\n\n"
+print(sen2)
